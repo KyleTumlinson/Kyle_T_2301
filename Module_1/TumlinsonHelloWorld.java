a849137fa66d4518ff112bcf7dc6e93eb41cff76
ktumlinson <52196694+ktumlinson@users.noreply.github.com>
2025-09-10 18:24:15 -0400

    Completed Hello World and brought in code_standards as a quick reference

---
+++ Module_1/TumlinsonHelloWorld.java
@@ -0,0 +1,8 @@
+public class TumlinsonHelloWorld {
+    public static void main(String[] args)
+    {
+        String firstName = "Kyle";
+        String lastName = "Tumlinson";
+        System.out.printf("Hello World from %s %s", firstName, lastName);
+    }
+}
